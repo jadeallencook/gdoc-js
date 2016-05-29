76d0bbaf2f488c454ba5ba4a836afc1833be68bf
Jade Allen Cook <jadeallencook@gmail.com>
2016-05-29 00:47:12 -0600

    🏁 Finished layout mode

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 window.onload = function() {
+    
     // caching public 
     var public = '1cfW7dwJkwJq7rqTsftNy3wjCJR3-yDylc5MRmsc0Yw8';
-    // init gDoc
+    
+    // example 1
     gDoc(public, 'layout');
-    // template insert
+    
+    // example 2
     gDoc({
         public: public,
         sheet: 'links',
-        id: 'links',
+        gDoc: 'links',
         data: ['title', 'link'],
+        loop: false,
         html: function() {
             var html = '<a href="{{link}}">{{title}}</a>';
             return html;
         }
     });
+    
 };
--- gDoc.js
+++ gDoc.js
@@ -6,31 +6,59 @@
  */
 
 window.gDoc = function (data, sheet) {
-    if (typeof (data) === 'object') {
-        // start cache
-        var id = data.id;
-        var array = data.data;
-        var html = data.html;
-        return false;
-    } else {
-        // using tabletop to get gDoc
-        function tabletop(doc) {
-            Tabletop.init({
-                key: doc,
-                callback: insertDoc,
-                simpleSheet: false
-            });
+    
+    var build = {
+        // build complete sheet
+        layout: function () {
+            // using tabletop to get gDoc
+            function tabletop(doc) {
+                Tabletop.init({
+                    key: doc,
+                    callback: insertDoc,
+                    simpleSheet: false
+                });
+            }
+            // inserting gDoc data
+            function insertDoc(obj) {
+                // grab sheet
+                obj = obj[sheet].elements[0];
+                // convert to array and get key/values
+                var keys = Object.keys(obj);
+                var values = Object.keys(obj).map(function (key) {
+                    return obj[key]
+                });
+                // loop through columns in gDoc
+                for (var i = 0; i < keys.length; i++) {
+                    // if the column has special selector
+                    if (keys[i].indexOf(':') > -1) {
+                        // split attribute and element
+                        var attribute = keys[i].substr(keys[i].indexOf(':') + 1);
+                        var element = keys[i].substr(0, keys[i].indexOf(':'));
+                        element = document.querySelector('[gDoc="' + element + '"]');
+                        element.setAttribute(attribute, values[i]);
+                    } else {
+                        // get elements that have gDocs set to column names
+                        var element = document.querySelector('[gDoc="' + keys[i] + '"]');
+                        // insert value into element
+                        element.innerHTML = values[i];
+                    }
+                }
+            }
+            // calling spreadsheet
+            tabletop(data);
+        },
+        // build block of html
+        block: function () {
+            // start cache
+            var id = data.id;
+            var array = data.data;
+            var html = data.html;
+            var public = data.public;
         }
-        // inserting gDoc data
-        function insertDoc(obj) {
-            // grab sheet
-            obj = obj[sheet].elements[0];
-            // convert to array
-            obj = Object.keys(obj).map(function (key) {
-                return obj[key]
-            });
-        }
-        // calling spreadsheet
-        tabletop(data);
-    }
+    };
+
+    // layout and block switch - based on var data 
+    if (typeof (data) === 'object') build.block();
+    else build.layout();
+    
 };
